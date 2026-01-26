585becfc4c90cc988a98d3644d124a950a67430e
Cristian Albertino de Lira Filho <cristianfilho2008@outlook.com>
2026-01-25 23:39:29 -0300

    Fix(loader): ajustar tamanho do Loader

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -10,7 +10,7 @@ function Loader({ loading }) {
         <Lottie
           animationData={pizzaJSON}
           loop={true}
-          style={{ width: 500, height: 500 }}
+          style={{ width: 300, height: 300 }}
         />
         <p className={styles.text}>Terminando de Assar a Pizza...</p>
       </div>
